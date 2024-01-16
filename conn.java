60e7daa8dfa3dd989be485f5373803e3c965e4a7
Aayush-19AM <84622618+Aayush-19AM@users.noreply.github.com>
2024-01-16 11:23:30 +0530

    Add files via upload

---
+++ conn.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class conn {
+
+    Connection c;
+    Statement s;
+
+    public conn(){
+
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "root");
+            s = c.createStatement();
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
